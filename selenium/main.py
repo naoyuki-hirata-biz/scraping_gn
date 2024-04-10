8c9687d4ad9f2cf4db335b70395ce502a099850c
Naoyuki Hirata <naoyuki.hirata.biz@gmail.com>
2024-04-10 15:16:48 +0900

    [fix]initial commit

---
+++ selenium/main.py
@@ -0,0 +1,191 @@
+import math
+import time
+import os
+import re
+import argparse
+import pandas as pd
+from datetime import datetime, timedelta, timezone
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+
+JST = timezone(timedelta(hours=+9), 'JST')
+
+def get_shop_urls(driver, target_url, limit=50):
+    urls = []
+    page = 1
+    max_page = math.ceil(limit / 20)  # 1ページあたり20件
+
+    wait = WebDriverWait(driver=driver, timeout=90)
+    driver.get(target_url)
+    while page <= max_page:
+        url_selector = 'article > div.style_title___HrjW > a.style_titleLink__oiHVJ'
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, url_selector)))
+        elems = driver.find_elements(By.CSS_SELECTOR, url_selector)
+        [urls.append(elems[i].get_attribute('href')) for i in range(len(elems))]
+        if len(urls) >= limit:
+            break
+
+        next_page_link_selector = '#__next > div > div.layout_body__LvaRc > main > div.style_pageNation__AZy1A > nav > ul > li:nth-last-child(2) > a'
+        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, next_page_link_selector)))
+        next_page_link = driver.find_element(By.CSS_SELECTOR, next_page_link_selector)
+        next_page_link.click()
+        page += 1
+        time.sleep(3)
+
+    return urls[:limit]
+
+def get_shop_details(driver, target_urls):
+    rows = []
+    wait = WebDriverWait(driver=driver, timeout=90)
+    limit = len(target_urls)
+    for i in range(limit):
+        row = []
+        target_url = target_urls[i]
+        SHOP_DETAIL_PAGE_ACCESS_RETRY_LIMIT = 3
+        has_error = True
+        for j in range(SHOP_DETAIL_PAGE_ACCESS_RETRY_LIMIT):
+          try:
+            driver.get(target_url)
+            has_error = False
+            break
+          except TimeoutException:
+              print('WARN ', datetime.now(JST), '     ', f'アクセス時にタイムアウトになりました({j + 1}回目)', target_url)
+              time.sleep(3)
+        if has_error:
+            print('ERROR', datetime.now(JST), '     ', 'アクセスできませんでした', target_url)
+            exit()
+
+        # 店舗名
+        shop_name_id = 'info-name'
+        wait.until(EC.visibility_of_element_located((By.ID, shop_name_id)))
+        elem = driver.find_element(By.ID, shop_name_id)
+        shop_name = elem.text
+        row.append(shop_name)
+        print('DEBUG', datetime.now(JST), f'{str(i + 1).rjust(len(str(limit)))}/{limit}', shop_name, target_url)
+        # 電話番号
+        tel_selector = '#info-phone span.number'
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, tel_selector)))
+        elem = driver.find_element(By.CSS_SELECTOR, tel_selector)
+        tel = elem.text
+        row.append(tel)
+        # メールアドレス
+        email = ''
+        elems = driver.find_elements(By.CSS_SELECTOR, '#info-table > table > tbody a[href^=mailto]')
+
+        if len(elems) > 0:
+            email = elems[0].get_attribute('href').replace('mailto:', '')
+        row.append(email)
+        # 住所
+        address = driver.find_element(By.CSS_SELECTOR, '#info-table > table > tbody p.adr > span.region').text
+        pattern = r'(東京都|北海道|(?:京都|大阪)府|.{2,3}県)?(.+?)(\d.*)'
+        match = re.match(pattern, address)
+        # 都道府県
+        row.append(match.group(1))
+        # 市区町村
+        row.append(match.group(2))
+        # 番地
+        row.append(match.group(3))
+        # 建物名
+        building = ''
+        elems = driver.find_elements(By.CSS_SELECTOR, '#info-table > table > tbody p.adr > span.locality')
+        if elems:
+            building = elems[0].text
+        row.append(building)
+        # URL, SSL
+        url = ''
+        ssl = ''
+        ## 「お店のホームページ」からURLを取得する
+        url_selector = '#info-table > table > tbody a.url'
+        elems = driver.find_elements(By.CSS_SELECTOR, url_selector)
+        LINK_CLICK_RETRY_LIMIT = 5
+        if elems:
+            # 動的にhref属性が生成されるまで待機
+            wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, f'{url_selector}:first-child[href^="http"]')))
+            for k in range(LINK_CLICK_RETRY_LIMIT):
+                try:
+                    # 別タブで開かれる
+                    elems[0].click()
+                    wait.until(EC.number_of_windows_to_be(2))
+                    handles = driver.window_handles
+                    driver.switch_to.window(handles[1])
+                    url = driver.current_url
+                    if url.startswith('https'):
+                        ssl = 'TRUE'
+                    else:
+                        ssl = 'FALSE'
+                    driver.close()
+                    driver.switch_to.window(handles[0])
+                    break
+                except TimeoutException:
+                    print('WARN ', datetime.now(JST), '     ', f'{shop_name}のお店のホームページから店舗URLを取得できませんでした({k + 1}回目)')
+                    time.sleep(3)
+
+        ## 「お店のホームページ」リンクからURLを取得できなかった場合、「オフィシャルページ」アイコンからURL取得を試みる
+        if url == '':
+            elems = driver.find_elements(By.CSS_SELECTOR, '#sv-site > li > a')
+            if len(elems) > 0:
+                for l in range(LINK_CLICK_RETRY_LIMIT):
+                    try:
+                        # 別タブで開かれる
+                        elems[0].click()
+                        wait.until(EC.number_of_windows_to_be(2))
+                        handles = driver.window_handles
+                        driver.switch_to.window(handles[1])
+                        url = driver.current_url
+                        if url.startswith('https'):
+                            ssl = 'TRUE'
+                        else:
+                            ssl = 'FALSE'
+                        driver.close()
+                        driver.switch_to.window(handles[0])
+                        break
+                    except TimeoutException:
+                        print('WARN ', datetime.now(JST), '     ', f'{shop_name}のオフィシャルページから店舗URLを取得できませんでした({l + 1}回目)')
+                        time.sleep(3)
+
+        row.append(url)
+        row.append(ssl)
+        rows.append(row)
+
+    return pd.DataFrame(rows, columns=['店舗名', '電話番号', 'メールアドレス', '都道府県', '市区町村', '番地', '建物名', 'URL', 'SSL'])
+
+def main():
+    OUTPUT_FILE_NAME = 'results.csv'
+    # 初期化
+    if (os.path.isfile(OUTPUT_FILE_NAME)):
+        os.remove(OUTPUT_FILE_NAME)
+
+    service = Service(executable_path='/usr/bin/chromedriver')
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(service=service, options=options)
+
+    # TARGET_URL = 'https://r.gnavi.co.jp/area/aream2115/kods00066/rs/?bdgMax=7000&character=KODS00143&sort=HIGH'
+    TARGET_URL = 'file:///opt/python/static/gnavi_list_01.html'
+
+    # 一覧の店舗詳細URLを取得
+    print('INFO ', datetime.now(JST), '検索結果の上位50件のURLを取得します')
+    shop_urls = get_shop_urls(driver, TARGET_URL)
+    print('INFO ', datetime.now(JST), '検索結果の上位50件のURLを取得しました')
+
+    print('INFO ', datetime.now(JST), '店舗詳細を取得します')
+    shop_details = get_shop_details(driver, shop_urls)
+    print('INFO ', datetime.now(JST), '店舗詳細を取得しました')
+
+    print('INFO ', datetime.now(JST), 'CSVファイルを出力します')
+    shop_details.to_csv(OUTPUT_FILE_NAME, encoding="utf-8_sig", index=False)
+    print('INFO ', datetime.now(JST), 'CSVファイルを出力しました')
+
+    driver.close()
+    driver.quit()
+
+# ==============================
+# メイン処理
+# ==============================
+main()
