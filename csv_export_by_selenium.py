b4a976dc1d99ae2f82f065dee4b7835f23c6d8cb
Naoyuki Hirata <naoyuki.hirata.biz@gmail.com>
2024-04-10 17:04:12 +0900

    [fix]use args

--- csv_export_by_selenium.py
+++ csv_export_by_selenium.py
@@ -14,19 +14,35 @@ from selenium.common.exceptions import TimeoutException
 
 JST = timezone(timedelta(hours=+9), 'JST')
 
-def get_shop_urls(driver, target_url, limit=50):
+def get_args():
+    parser = argparse.ArgumentParser(description='Usage')
+    parser.add_argument('--filename', help='output csv filename (default: results_by_selenium.csv)', type=str)
+    parser.add_argument('--uri', help='gnavi url (default: static files)', type=str)
+    parser.add_argument('--shops', help='Maximum number of shops acquired (default: 50)', type=int)
+    parser.add_argument('--timeout', help='Timeout time to find the element (seconds) (default: 90)', type=int)
+    parser.add_argument('--retry', help='Number of retries (default: 3)', type=int)
+
+    args = parser.parse_args()
+    args.filename = args.filename or 'results_by_selenium.csv'
+    args.uri = args.uri or 'file:///opt/python/static/gnavi_list_01.html'
+    args.shops = args.shops or 50
+    args.timeout = args.timeout or 90
+    args.retry = args.retry or 3
+    return vars(args)
+
+def get_shop_urls(driver, **kwargs):
     urls = []
     page = 1
-    max_page = math.ceil(limit / 20)  # 1ページあたり20件
+    max_page = math.ceil(kwargs['shops'] / 20)  # 1ページあたり20件
 
     wait = WebDriverWait(driver=driver, timeout=90)
-    driver.get(target_url)
+    driver.get(kwargs['uri'])
     while page <= max_page:
         url_selector = 'article > div.style_title___HrjW > a.style_titleLink__oiHVJ'
         wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, url_selector)))
         elems = driver.find_elements(By.CSS_SELECTOR, url_selector)
         [urls.append(elems[i].get_attribute('href')) for i in range(len(elems))]
-        if len(urls) >= limit:
+        if len(urls) >= kwargs['shops']:
             break
 
         next_page_link_selector = '#__next > div > div.layout_body__LvaRc > main > div.style_pageNation__AZy1A > nav > ul > li:nth-last-child(2) > a'
@@ -36,18 +52,17 @@ def get_shop_urls(driver, target_url, limit=50):
         page += 1
         time.sleep(3)
 
-    return urls[:limit]
+    return urls[:kwargs['shops']]
 
-def get_shop_details(driver, target_urls):
+def get_shop_details(driver, target_urls, **kwargs):
     rows = []
-    wait = WebDriverWait(driver=driver, timeout=90)
+    wait = WebDriverWait(driver=driver, timeout=kwargs['timeout'])
     limit = len(target_urls)
     for i in range(limit):
         row = []
         target_url = target_urls[i]
-        SHOP_DETAIL_PAGE_ACCESS_RETRY_LIMIT = 3
         has_error = True
-        for j in range(SHOP_DETAIL_PAGE_ACCESS_RETRY_LIMIT):
+        for j in range(kwargs['retry']):
           try:
             driver.get(target_url)
             has_error = False
@@ -101,11 +116,10 @@ def get_shop_details(driver, target_urls):
         ## 「お店のホームページ」からURLを取得する
         url_selector = '#info-table > table > tbody a.url'
         elems = driver.find_elements(By.CSS_SELECTOR, url_selector)
-        LINK_CLICK_RETRY_LIMIT = 5
         if elems:
             # 動的にhref属性が生成されるまで待機
             wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, f'{url_selector}:first-child[href^="http"]')))
-            for k in range(LINK_CLICK_RETRY_LIMIT):
+            for k in range(kwargs['retry']):
                 try:
                     # 別タブで開かれる
                     elems[0].click()
@@ -128,7 +142,7 @@ def get_shop_details(driver, target_urls):
         if url == '':
             elems = driver.find_elements(By.CSS_SELECTOR, '#sv-site > li > a')
             if len(elems) > 0:
-                for l in range(LINK_CLICK_RETRY_LIMIT):
+                for l in range(kwargs['retry']):
                     try:
                         # 別タブで開かれる
                         elems[0].click()
@@ -153,11 +167,13 @@ def get_shop_details(driver, target_urls):
 
     return pd.DataFrame(rows, columns=['店舗名', '電話番号', 'メールアドレス', '都道府県', '市区町村', '番地', '建物名', 'URL', 'SSL'])
 
+# https://r.gnavi.co.jp/area/aream2115/kods00066/rs/?bdgMax=7000&character=KODS00143&sort=HIGH
 def main():
-    OUTPUT_FILE_NAME = 'results.csv'
+    args = get_args()
+
     # 初期化
-    if (os.path.isfile(OUTPUT_FILE_NAME)):
-        os.remove(OUTPUT_FILE_NAME)
+    if (os.path.isfile(args['filename'])):
+        os.remove(args['filename'])
 
     service = Service(executable_path='/usr/bin/chromedriver')
     options = webdriver.ChromeOptions()
@@ -166,20 +182,17 @@ def main():
     options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(service=service, options=options)
 
-    # TARGET_URL = 'https://r.gnavi.co.jp/area/aream2115/kods00066/rs/?bdgMax=7000&character=KODS00143&sort=HIGH'
-    TARGET_URL = 'file:///opt/python/static/gnavi_list_01.html'
-
     # 一覧の店舗詳細URLを取得
-    print('INFO ', datetime.now(JST), '検索結果の上位50件のURLを取得します')
-    shop_urls = get_shop_urls(driver, TARGET_URL)
-    print('INFO ', datetime.now(JST), '検索結果の上位50件のURLを取得しました')
+    print('INFO ', datetime.now(JST), f"検索結果の上位{args['shops']}件のURLを取得します")
+    shop_urls = get_shop_urls(driver, **args)
+    print('INFO ', datetime.now(JST), f"検索結果の上位{args['shops']}件のURLを取得しました")
 
     print('INFO ', datetime.now(JST), '店舗詳細を取得します')
-    shop_details = get_shop_details(driver, shop_urls)
+    shop_details = get_shop_details(driver, shop_urls, **args)
     print('INFO ', datetime.now(JST), '店舗詳細を取得しました')
 
     print('INFO ', datetime.now(JST), 'CSVファイルを出力します')
-    shop_details.to_csv(OUTPUT_FILE_NAME, encoding="utf-8_sig", index=False)
+    shop_details.to_csv(args['filename'], encoding="utf-8_sig", index=False)
     print('INFO ', datetime.now(JST), 'CSVファイルを出力しました')
 
     driver.close()
