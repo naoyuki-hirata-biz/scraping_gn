57c7082ca2c2eb1335f390bab5dc7d1d3f7b9dd0
Naoyuki Hirata <naoyuki.hirata.biz@gmail.com>
2024-04-12 17:52:52 +0900

    [fix]optional uri

--- README.md
+++ README.md
@@ -2,17 +2,17 @@
 [![Python Version](https://img.shields.io/badge/python-3.9-brightgreen.svg)](https://python.org)
 
 ```
-usage: csv_export.py [-h] --uri URI [--lib {requests,selenium}] [--filename FILENAME] [--shops SHOPS] [--timeout TIMEOUT] [--retry RETRY]
+usage: csv_export.py [-h] [--uri URI] [--lib {requests,selenium}] [--filename FILENAME] [--shops SHOPS] [--timeout TIMEOUT] [--retry RETRY]
 
 Usage
 
 optional arguments:
   -h, --help            show this help message and exit
-  --uri URI             gnavi url (If using static files, specify 'file:///opt/python/static/html/gnavi_list_01.html')
+  --uri URI             gnavi url (default: 'file:///opt/python/static/html/gnavi_list_01.html')
   --lib {requests,selenium}
-                        use requests or selenium library (default: selenium)
+                        use requests or selenium library (default: requests)
   --filename FILENAME   output csv filename (default: results.csv)
-  --shops SHOPS         Maximum number of shops acquired (default: 50)
+  --shops SHOPS         Maximum number of shops acquired (default: 50, max: 50)
   --timeout TIMEOUT     Timeout time to find the element (seconds) (default: 90)
   --retry RETRY         Number of retries (default: 3)
   ```
--- csv_export.py
+++ csv_export.py
@@ -5,16 +5,16 @@ from builder import CsvBuilderFactory
 
 def get_args():
     parser = argparse.ArgumentParser(description='Usage')
-    parser.add_argument('--uri', help="gnavi url (If using static files, specify 'file:///opt/python/static/html/gnavi_list_01.html')", required=True, type=str)
-    # TODO: default: requests
-    parser.add_argument('--lib', help='use requests or selenium library (default: selenium)', choices=['requests', 'selenium'])
+    parser.add_argument('--uri', help="gnavi url (default: 'file:///opt/python/static/html/gnavi_list_01.html')", type=str)
+    parser.add_argument('--lib', help='use requests or selenium library (default: requests)', choices=['requests', 'selenium'])
     parser.add_argument('--filename', help='output csv filename (default: results.csv)', type=str)
     parser.add_argument('--shops', help='Maximum number of shops acquired (default: 50, max: 50)', type=int)
     parser.add_argument('--timeout', help='Timeout time to find the element (seconds) (default: 90)', type=int)
     parser.add_argument('--retry', help='Number of retries (default: 3)', type=int)
 
     args = parser.parse_args()
-    args.lib = args.lib or 'selenium'
+    args.uri = args.uri or 'file:///opt/python/static/html/gnavi_list_01.html'
+    args.lib = args.lib or 'requests'
     args.filename = args.filename or 'results.csv'
     if args.shops is None or args.shops < 1 or args.shops > 50:
         args.shops = 50
