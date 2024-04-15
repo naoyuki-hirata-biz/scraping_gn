3055a8dadd50e448fb727b770bc264752d4be552
Naoyuki Hirata <naoyuki.hirata.biz@gmail.com>
2024-04-15 14:03:23 +0000

    [fix]require uri

--- csv_export.py
+++ csv_export.py
@@ -5,7 +5,7 @@ Usage
 
 optional arguments:
   -h, --help            show this help message and exit
-  --uri URI             gnavi url (default: 'file:///opt/python/static/html/gnavi_list_01.html')
+  --uri URI             gnavi url
   --lib {requests,selenium}
                         use requests or selenium library (default: requests)
   --filename FILENAME   output csv filename (default: results.csv)
@@ -20,9 +20,9 @@ from csv_creator import CsvCreatorFactory
 
 
 def get_args():
-    """Return Arguments."""
+    """Return arguments."""
     parser = argparse.ArgumentParser(description='Usage')
-    parser.add_argument('--uri', help="gnavi url (default: 'file:///opt/python/static/html/gnavi_list_01.html')", type=str)
+    parser.add_argument('--uri', help='gnavi url', required=True, type=str)
     parser.add_argument('--lib', help='use requests or selenium library (default: requests)', choices=['requests', 'selenium'])
     parser.add_argument('--filename', help='output csv filename (default: results.csv)', type=str)
     parser.add_argument('--shops', help='Maximum number of shops acquired (default: 50, max: 50)', type=int)
@@ -30,7 +30,6 @@ def get_args():
     parser.add_argument('--retry', help='Number of retries (default: 3)', type=int)
 
     args = parser.parse_args()
-    args.uri = args.uri or 'file:///opt/python/static/html/gnavi_list_01.html'
     args.lib = args.lib or 'requests'
     args.filename = args.filename or 'results.csv'
     if args.shops is None or args.shops < 1 or args.shops > 50:
@@ -41,14 +40,13 @@ def get_args():
     return vars(args)
 
 
-# ==============================
-# メイン処理
-# ==============================
 def main():
-  arguments = get_args()
+    """Main processing."""
+    arguments = get_args()
 
-  creator = CsvCreatorFactory().create_csv_creator(**arguments)
-  creator.create()
+    creator = CsvCreatorFactory().create_csv_creator(**arguments)
+    creator.create()
+
 
 if __name__ == '__main__':
-  main()
+    main()
