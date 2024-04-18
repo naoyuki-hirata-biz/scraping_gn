b6b7acf532390b1c01d18d319fad02b026507d3d
Naoyuki Hirata <naoyuki.hirata.biz@gmail.com>
2024-04-18 08:23:24 +0000

    [fix]default values of args

--- csv_export.py
+++ csv_export.py
@@ -12,6 +12,13 @@ optional arguments:
   --shops SHOPS         Maximum number of shops acquired (default: 50, max: 50)
   --timeout TIMEOUT     Timeout time to find the element (seconds) (default: 90)
   --retry RETRY         Number of retries (default: 3)
+
+
+Test
+
+pytest tests/test_create_csv_by_requests.py -s
+pytest tests/test_create_csv_by_selenium.py -s
+
 """
 
 import argparse
@@ -23,20 +30,15 @@ def get_args():
     """Return arguments."""
     parser = argparse.ArgumentParser(description='Usage')
     parser.add_argument('--uri', help='gnavi url', required=True, type=str)
-    parser.add_argument('--lib', help='use requests or selenium library (default: requests)', choices=['requests', 'selenium'])
-    parser.add_argument('--filename', help='output csv filename (default: results.csv)', type=str)
-    parser.add_argument('--shops', help='Maximum number of shops acquired (default: 50, max: 50)', type=int)
-    parser.add_argument('--timeout', help='Timeout time to find the element (seconds) (default: 90)', type=int)
-    parser.add_argument('--retry', help='Number of retries (default: 3)', type=int)
+    parser.add_argument('--lib', help='use requests or selenium library (default: requests)', choices=['requests', 'selenium'], default='requests')
+    parser.add_argument('--filename', help='output csv filename (default: results.csv)', type=str, default='results.csv')
+    parser.add_argument('--shops', help='Maximum number of shops acquired (default: 50, max: 50)', type=int, default=50)
+    parser.add_argument('--timeout', help='Timeout time to find the element (seconds) (default: 90)', type=int, default=90)
+    parser.add_argument('--retry', help='Number of retries (default: 3)', type=int, default=3)
 
     args = parser.parse_args()
-    args.lib = args.lib or 'requests'
-    args.filename = args.filename or 'results.csv'
-    if args.shops is None or args.shops < 1 or args.shops > 50:
+    if args.shops < 1 or args.shops > 50:
         args.shops = 50
-
-    args.timeout = args.timeout or 90
-    args.retry = args.retry or 3
     return vars(args)
 
 
